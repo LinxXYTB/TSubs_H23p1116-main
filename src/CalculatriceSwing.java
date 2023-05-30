c1c09a00a4a1cab2c2a2ec020152d3398e3471b6
Iulian Moraru <iulianghitiu0102@gmail.com>
2023-05-29 22:37:03 -0400

    TODO 15 - 16

--- src/CalculatriceSwing.java
+++ src/CalculatriceSwing.java
@@ -1,5 +1,15 @@
 // TODOs 11 à 14
 
+import javax.swing.JFrame;
+
 public class CalculatriceSwing extends JFrame {
-   
+    public CalculatriceSwing() {
+    
+        this.setTitle("Calculatrice");
+        this.setSize(300, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.io.*;
+import java.util.*;
+
 public class Main {
 	// TODOs 15 : public static List<Personne> lireFichier(String fichier) 
     // TODO 16: public static void ecrireFichier(List<Personne> personnes, String fichier)
@@ -18,6 +22,42 @@ public class Main {
         }
         public String getCourriel() {
             return courriel;
+        }
+    }
+
+    public static List<Main.Personne> lireFichier(String fichier) {
+        List<Personne> personnes = new ArrayList<>();
+        try {
+            File fichierALire = new File(fichier);
+            Scanner scan = new Scanner(fichierALire);
+            while (scan.hasNextLine()) {
+                String ligne = scan.nextLine();
+                String[] elements = ligne.split(",");
+                String nom = elements[0];
+                int age = Integer.parseInt(elements[1]);
+                String email = elements[2];
+                Personne personne = new Personne(nom, age, email);
+                personnes.add(personne);
+            }
+            scan.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return personnes;
+    }
+    public static void ecrireFichier(List<Personne> personnes, String fichierSortie) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fichierSortie))) {
+            writer.write("<html><body>\n");
+            for (Personne personne : personnes) {
+                writer.write("<p>\n");
+                writer.write("Nom: <strong>" + personne.getNom() + "</strong><br>\n");
+                writer.write("Age: " + personne.getAge() + "<br>\n");
+                writer.write("Email: <a href=\"mailto:" + personne.getCourriel() + "\">" + personne.getCourriel() + "</a><br>\n");
+                writer.write("</p>\n");
+            }
+            writer.write("</body></html>\n");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
      
